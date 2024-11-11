80091f6830be9748c20ca67fd9261b599938ba0c
eddyoluko <30697198-eddyoluko@users.noreply.replit.com>
2024-11-11 00:16:05 +0000

    Agent query: Can you try the password reset functionality by entering a registered email address and verify if you receive the reset email?

--- app.py
+++ app.py
@@ -28,7 +28,10 @@ app.config['MAIL_PORT'] = 587
 app.config['MAIL_USE_TLS'] = True
 app.config['MAIL_USERNAME'] = os.environ.get('MAIL_USERNAME')
 app.config['MAIL_PASSWORD'] = os.environ.get('MAIL_PASSWORD')
-app.config['MAIL_DEFAULT_SENDER'] = os.environ.get('MAIL_USERNAME')
+app.config['MAIL_DEFAULT_SENDER'] = (
+    'User Registration System',
+    os.environ.get('MAIL_USERNAME')
+)
 
 mail = Mail(app)
 
